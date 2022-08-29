5924409ef584dbc88644fb45292a396818e4a532
alaqqad <alialaqqad12@gmail.com>
2022-08-29 16:19:09 +0300

    Add resetBoard function and make the draw just once click on the board in script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let color ="black";
+let color ="black"; // we put this as default color, to use it later and change it in functions(changeColor and colorSquare)
+let click = true; // this to make only the click mouseover happens when we click
+
 
 function populateBoard(size) {
     let board = document.querySelector('.board');
@@ -26,5 +28,28 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-    this.style.background = color; // we used (this) because if we use square.style.background it will show that square is not defined. thus we can use square as global variable but we need square in the loop, so we put (this) to solve the issue // ex: if we have Ali = 26. then : this. will show 26, as it will show the what is the value equal.
 }
+function colorSquare() {
+    if (!click) { //this with click value top, and event listener below to make only the click mouseover happens when we click
+        if(color == 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.background = color; // we used (this) because if we use square.style.background it will show that square is not defined. thus we can use square as global variable but we need square in the loop, so we put (this) to solve the issue // ex: if we have Ali = 26. then : this. will show 26, as it will show the what is the value equal.
+        }
+    }
+
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector("body").addEventListener("click", () => {
+    click = !click;
+})
